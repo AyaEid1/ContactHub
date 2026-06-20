f869802e6c78a749fef88bbc6e75d89d743f9043
AyaEid1 <ayaeid1995@gmail.com>
2026-06-21 01:04:13 +0300

    .........

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,8 @@ var inputNotes = document.getElementById("input-notes");
 var favoriteInput = document.getElementById("favorite");
 var emergencyInput = document.getElementById("emergency");
 var saveBtn = document.getElementById("save-btn");
+var saveChangesBtn = document.getElementById("save-changes-btn");
+var currentEditIndex = 0;
 var contactInfoList = [];
 
 if (localStorage.getItem("containerContact")  !== null) {
@@ -34,10 +36,10 @@ function addContact() {
       .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
       .join(" "),
     numberUser: inputNumber.value.trim(),
-    emailUser: inputEmail.value,
-    locationUser: inputLocation.value,
-    selectGroupUser: selectGroup.value,
-    notesUser: inputNotes.value,
+    emailUser: inputEmail.value.trim(),
+    locationUser: inputLocation.value.trim(),
+    selectGroupUser: selectGroup.value.trim(),
+    notesUser: inputNotes.value.trim(),
     favorite: favoriteInput.checked,
     emergency: emergencyInput.checked,
   };
@@ -64,130 +66,7 @@ function displayContact() {
   }
   var containerContact = "";
   for (var i = 0; i < contactInfoList.length; i++) {
-    containerContact += `
-      <div class="col-lg-6 mb-4">
-                  <div class="contact-card">
-                    <div class="contact-body">
-                      <div class="d-flex align-items-start gap-3">
-                        <div class="avatar-wrapper">
-                          <div class="avatar-contact">${contactInfoList[i].initials}</div>
-                          ${
-                            contactInfoList[i].emergency
-                              ? `<span class="heart-badge">
-   <i class="fas fa-heart-pulse"></i>
- </span>`
-                              : ""
-                          }
-
-                          ${
-                            contactInfoList[i].favorite
-                              ? `    <span class="favorite-badge">  <i class="fas fa-star"></i>
-                          </span>`
-                              : ""
-                          }
-                         
-                        
-                        </div>
-
-                        <div class="flex-grow-1">
-                          <h5 class="contact-name">${contactInfoList[i].nameUser}</h5>
-
-                          <div class="contact-item">
-                            <span class="info-icon phone">
-                              <i class="fas fa-phone"></i>
-                            </span>
-
-                            <span>${contactInfoList[i].numberUser}</span>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="anther-info">
-                       ${
-                         contactInfoList[i].emailUser
-                           ? ` 
-                        <div class="contact-item">
-                          <span class="info-icon email">
-                            <i class="fas fa-envelope"></i>
-                          </span> 
-                          <span>${contactInfoList[i].emailUser}</span>
-                        </div> `
-                           : ""
-                       }
-
-                       ${
-                         contactInfoList[i].locationUser
-                           ? ` <div class="contact-item">
-                           <span class="info-icon location">
-                             <i class="fas fa-location-dot"></i>
-                           </span>
-
-                           <span>${contactInfoList[i].locationUser}</span>
-                         </div>`
-                           : ""
-                       }
-                     
-                      </div>
-                      <div class="contact-tags">
-                      ${
-                        contactInfoList[i].selectGroupUser
-                          ? `<span class="tag school"> ${contactInfoList[i].selectGroupUser} </span> `
-                          : ""
-                      }
-                         
-                          ${
-                            contactInfoList[i].emergency
-                              ? `<span class="tag emergency"> <i class="fas fa-heart-pulse"></i>
-                              Emergency
-                          </span>`
-                              : ""
-                          }
-                          
-                     
-                        </div>
-                    </div>
-
-                    <div class="contact-footer">
-                      <div class="left-actions">
-                      ${
-                        contactInfoList[i].numberUser
-                          ? ` <a class="action-btn call" href="tel:${contactInfoList[i].numberUser}">
-                          <i class="fas fa-phone"></i>
-                        </a> `
-                          : ""
-                      }
-                       ${
-                         contactInfoList[i].emailUser
-                           ? ` <a class="action-btn mail" href="mailto:${contactInfoList[i].emailUser}">
-                          <i class="fas fa-envelope"></i>
-                        </a>`
-                           : ""
-                       }
-                       
-                      </div>
-
-                      <div class="right-actions">
-                        <button class="action-btn favorite">
-                          <i class="fas fa-star"></i>
-                        </button>
-
-                        <button class="action-btn heart">
-                          <i class="fas fa-heart-pulse"></i>
-                        </button>
-
-                        <button class="action-btn edit"data-bs-toggle="modal"
-              data-bs-target="#exampleModal">
-                          <i class="fas fa-pen"></i>
-                        </button>
-
-                        <button class="action-btn delete" onclick="deleteContact(${i})">
-                          <i class="fas fa-trash"></i>
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-     
-     `;
+    containerContact += getContainerContact(i);
   }
   document.getElementById("contactsList").innerHTML = containerContact;
 }
@@ -213,8 +92,14 @@ function searchContact() {
   var searchValue = inputSearch.value.trim().toLowerCase(); 
   var containerContact = "";
   for (var i = 0; i < contactInfoList.length; i++) {
-     if (contactInfoList[i].nameUser.trim().toLowerCase().includes(searchValue)== true) {
-    containerContact += `
+     if (contactInfoList[i].nameUser.trim().toLowerCase().includes(searchValue)== true || contactInfoList[i].numberUser.trim().toLowerCase().includes(searchValue)== true || contactInfoList[i].emailUser.trim().toLowerCase().includes(searchValue)== true) {
+    containerContact +=  getContainerContact(i);
+  }
+  document.getElementById("contactsList").innerHTML = containerContact; 
+ } 
+}
+function getContainerContact(i){
+  return `
       <div class="col-lg-6 mb-4">
                   <div class="contact-card">
                     <div class="contact-body">
@@ -240,7 +125,7 @@ function searchContact() {
                         </div>
 
                         <div class="flex-grow-1">
-                          <h5 class="contact-name">${contactInfoList[i].nameUser}</h5>
+                          <h5 class="contact-name">${contactInfoList[i].nameUser.split(' ').slice(0, 2).join(' ')}</h5>
 
                           <div class="contact-item">
                             <span class="info-icon phone">
@@ -324,7 +209,7 @@ function searchContact() {
                           <i class="fas fa-heart-pulse"></i>
                         </button>
 
-                        <button class="action-btn edit"data-bs-toggle="modal"
+                        <button class="action-btn edit" onclick="editContact(${i})" data-bs-toggle="modal"
               data-bs-target="#exampleModal">
                           <i class="fas fa-pen"></i>
                         </button>
@@ -338,7 +223,49 @@ function searchContact() {
                 </div>
      
      `;
-  }
-  document.getElementById("contactsList").innerHTML = containerContact; 
- } 
+}
+function editContact(index) {
+  currentEditIndex = index;
+   inputName.value = contactInfoList[index].nameUser;
+  inputNumber.value = contactInfoList[index].numberUser;
+  inputEmail.value = contactInfoList[index].emailUser;
+  inputLocation.value = contactInfoList[index].locationUser;
+  selectGroup.value = contactInfoList[index].selectGroupUser;
+  inputNotes.value = contactInfoList[index].notesUser; ;
+favoriteInput.checked = contactInfoList[index].favorite;
+  emergencyInput.checked = contactInfoList[index].emergency;
+    saveBtn.classList.add("d-none");
+   saveChangesBtn.classList.remove("d-none");
+  
+}
+
+function saveEditContact() { 
+   var contact = { 
+    initials: inputName.value
+      .trim()
+      .split(" ")
+      .slice(0, 2)
+      .map((word) => word.charAt(0).toUpperCase())
+      .join(""),
+    nameUser: inputName.value
+      .trim()
+      .split(" ")
+      .slice(0, 2)
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+      .join(" "),
+    numberUser: inputNumber.value.trim(),
+    emailUser: inputEmail.value.trim(),
+    locationUser: inputLocation.value.trim(),
+    selectGroupUser: selectGroup.value,
+    notesUser: inputNotes.value.trim(),
+    favorite: favoriteInput.checked,
+    emergency: emergencyInput.checked,
+  };
+  contactInfoList.splice(currentEditIndex, 1, contact);
+  localStorage.setItem("containerContact", JSON.stringify(contactInfoList));
+  displayContact();
+  resetContact();
+  saveBtn.classList.remove("d-none"); ;
+  saveChangesBtn.classList.add("d-none");
+
 }
