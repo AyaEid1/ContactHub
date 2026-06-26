0a41494eedeb2d935f8a888820e578fe4ecaebd6
AyaEid1 <ayaeid1995@gmail.com>
2026-06-27 01:06:13 +0300

    End task

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,7 @@
-// import Swal from "sweetalert2";
-// const Swal = require("sweetalert2");
-// var PhotoInput = document.getElementById("input-photo");
 var inputSearch = document.getElementById("searchInput");
-var inputName = document.getElementById("input-name");
-var inputNumber = document.getElementById("input-number");
-var inputEmail = document.getElementById("input-email");
+var inputName = document.getElementById("inputName");
+var inputNumber = document.getElementById("inputNumber");
+var inputEmail = document.getElementById("inputEmail");
 var inputLocation = document.getElementById("input-adress");
 var selectGroup = document.getElementById("select-group");
 var inputNotes = document.getElementById("input-notes");
@@ -15,12 +12,27 @@ var saveChangesBtn = document.getElementById("save-changes-btn");
 var currentEditIndex = 0;
 var contactInfoList = [];
 
-if (localStorage.getItem("containerContact")  !== null) {
+if (localStorage.getItem("containerContact") !== null) {
    contactInfoList = JSON.parse(localStorage.getItem("containerContact"));
-  displayContact();
- 
-} 
+  displayContact(); 
+}  
+displayFavorites();
+displayEmergency();
+updateStatistics();
+
 function addContact() {
+  if(validation(inputName,"contactNameError") && validation(inputNumber,"contactPhoneError") && validation(inputEmail,"contactEmailError")){  
+     if (isContactExist(inputNumber.value.trim())) {
+
+          Swal.fire({
+              title: "Contact Exists",
+              text: "This phone number already exists.",
+              icon: "warning"
+          });
+
+          return;
+      }
+
   var contact = {
     // PhotoInput: "assets/images/avatar.png",
     initials: inputName.value
@@ -42,15 +54,29 @@ function addContact() {
     notesUser: inputNotes.value.trim(),
     favorite: favoriteInput.checked,
     emergency: emergencyInput.checked,
-  };
+  }; 
   contactInfoList.push(contact);
   localStorage.setItem("containerContact", JSON.stringify(contactInfoList));
   displayContact();
+  displayFavorites();
+displayEmergency();
+  updateStatistics();
   resetContact();
+  Swal.fire({
+  title: 'Added',
+  text: 'Contact has been added successfully.',
+  icon: 'success', 
+})
+  }else{
+    Swal.fire({
+  title: 'Error',
+  text: 'Please fill in the required fields correctly.',
+  icon: 'error', 
+})
+  }
 }
 function resetContact() {
-  inputName.value = null;
-  inputName.value = null;
+  inputName.value = null; 
   inputNumber.value = null;
   inputEmail.value = null;
   inputLocation.value = null;
@@ -58,6 +84,14 @@ function resetContact() {
   inputNotes.value = null;
   favoriteInput.checked = false;
   emergencyInput.checked = false;
+  inputName.classList.remove("is-valid", "is-invalid");
+  inputNumber.classList.remove("is-valid", "is-invalid");
+  inputEmail.classList.remove("is-valid", "is-invalid");
+  document.getElementById("contactNameError").classList.add("d-none");
+  document.getElementById("contactPhoneError").classList.add("d-none");
+  document.getElementById("contactEmailError").classList.add("d-none");
+  saveBtn.classList.remove("d-none"); 
+saveChangesBtn.classList.add("d-none");
 }
 function displayContact() {
   if (contactInfoList.length === 0) {
@@ -74,6 +108,9 @@ function deleteContact(index) {
   contactInfoList.splice(index, 1);
   localStorage.setItem("containerContact", JSON.stringify(contactInfoList));
    displayContact(); 
+   displayFavorites();
+displayEmergency();
+   updateStatistics();
 }
 function showEmptyState() {
   document.getElementById("contactsList").innerHTML = `
@@ -95,8 +132,8 @@ function searchContact() {
      if (contactInfoList[i].nameUser.trim().toLowerCase().includes(searchValue)== true || contactInfoList[i].numberUser.trim().toLowerCase().includes(searchValue)== true || contactInfoList[i].emailUser.trim().toLowerCase().includes(searchValue)== true) {
     containerContact +=  getContainerContact(i);
   }
-  document.getElementById("contactsList").innerHTML = containerContact; 
- } 
+ }
+  document.getElementById("contactsList").innerHTML = containerContact;  
 }
 function getContainerContact(i){
   return `
@@ -201,11 +238,15 @@ function getContainerContact(i){
                       </div>
 
                       <div class="right-actions">
-                        <button class="action-btn favorite">
+                        <button class="action-btn favorite ${
+  contactInfoList[i].favorite ? 'active' : ''
+}" onclick="toggleFavorite(${i})">
                           <i class="fas fa-star"></i>
                         </button>
 
-                        <button class="action-btn heart">
+                        <button class="action-btn heart ${
+  contactInfoList[i].emergency ? 'active' : ''
+}" onclick="toggleEmergency(${i})">
                           <i class="fas fa-heart-pulse"></i>
                         </button>
 
@@ -239,33 +280,322 @@ favoriteInput.checked = contactInfoList[index].favorite;
   
 }
 
-function saveEditContact() { 
-   var contact = { 
+// function saveEditContact() { 
+  
+//     if (isContactExist(inputNumber.value.trim(), currentEditIndex)) {
+
+//     Swal.fire({
+//       title: "Contact Exists",
+//       text: "This phone number already exists.",
+//       icon: "warning"
+//     });
+
+//     return;
+//   }
+//    var contact = { 
+//     initials: inputName.value
+//       .trim()
+//       .split(" ")
+//       .slice(0, 2)
+//       .map((word) => word.charAt(0).toUpperCase())
+//       .join(""),
+//     nameUser: inputName.value
+//       .trim()
+//       .split(" ")
+//       .slice(0, 2)
+//       .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+//       .join(" "),
+//     numberUser: inputNumber.value.trim(),
+//     emailUser: inputEmail.value.trim(),
+//     locationUser: inputLocation.value.trim(),
+//     selectGroupUser: selectGroup.value,
+//     notesUser: inputNotes.value.trim(),
+//     favorite: favoriteInput.checked,
+//     emergency: emergencyInput.checked,
+//   };
+//   contactInfoList.splice(currentEditIndex, 1, contact);
+//   isContactExist(
+//     inputNumber.value.trim(),
+//     currentEditIndex
+// )
+//   localStorage.setItem("containerContact", JSON.stringify(contactInfoList));
+//   displayContact();
+//   resetContact();
+//   saveBtn.classList.remove("d-none"); ;
+//   saveChangesBtn.classList.add("d-none");
+ 
+// }
+
+
+function saveEditContact() {
+
+  if (
+    !validation(inputName, "contactNameError") ||
+    !validation(inputNumber, "contactPhoneError") ||
+    !validation(inputEmail, "contactEmailError")
+  ) {
+    Swal.fire({
+      title: "Error",
+      text: "Please fill in the required fields correctly.",
+      icon: "error"
+    });
+
+    return;
+  }
+
+  if (isContactExist(inputNumber.value.trim(), currentEditIndex)) {
+    Swal.fire({
+      title: "Contact Exists",
+      text: "This phone number already exists.",
+      icon: "warning"
+    });
+
+    return;
+  }
+
+  var contact = {
     initials: inputName.value
       .trim()
       .split(" ")
       .slice(0, 2)
-      .map((word) => word.charAt(0).toUpperCase())
+      .map(word => word.charAt(0).toUpperCase())
       .join(""),
+
     nameUser: inputName.value
       .trim()
       .split(" ")
       .slice(0, 2)
-      .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
       .join(" "),
+
     numberUser: inputNumber.value.trim(),
     emailUser: inputEmail.value.trim(),
     locationUser: inputLocation.value.trim(),
-    selectGroupUser: selectGroup.value,
+    selectGroupUser: selectGroup.value.trim(),
     notesUser: inputNotes.value.trim(),
     favorite: favoriteInput.checked,
     emergency: emergencyInput.checked,
   };
+
   contactInfoList.splice(currentEditIndex, 1, contact);
-  localStorage.setItem("containerContact", JSON.stringify(contactInfoList));
+
+  localStorage.setItem(
+    "containerContact",
+    JSON.stringify(contactInfoList)
+  );
+
   displayContact();
+  displayFavorites();
+displayEmergency();
+  updateStatistics();
   resetContact();
-  saveBtn.classList.remove("d-none"); ;
+
+  saveBtn.classList.remove("d-none");
   saveChangesBtn.classList.add("d-none");
 
+  Swal.fire({
+    title: "Updated",
+    text: "Contact updated successfully.",
+    icon: "success"
+  });
+}
+updateStatistics()
+function validation(element ,msId){
+var text = element.value;
+var regex = { 
+inputName:/^[A-Za-z\s]{2,50}$/,
+inputNumber:/^01[0125][0-9]{8}$/,
+inputEmail:/^$|[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+$/
+}
+var msId =document.getElementById(msId)
+if(regex[element.id].test(text)){
+element.classList.add("is-valid");
+element.classList.remove("is-invalid");
+msId.classList.add("d-none");
+return true
+}
+else{
+element.classList.add("is-invalid");
+element.classList.remove("is-valid");
+msId.classList.remove("d-none");
+return false
+}
+}
+/***********/
+ function displayFavorites() {
+
+    var container = "";
+
+    for (var i = 0; i < contactInfoList.length; i++) {
+
+        if (contactInfoList[i].favorite == true) {
+
+            container += `
+            
+            <div class="favorite-item d-flex justify-content-between align-items-center mb-3">
+
+                <div class="d-flex align-items-center gap-2">
+
+                    <div class="avatar-contact">
+                        ${contactInfoList[i].initials}
+                    </div>
+
+                    <div class="side-info">
+
+                        <h6 class="side-name">
+                            ${contactInfoList[i].nameUser}
+                        </h6>
+
+                        <small class="side-number">
+                            ${contactInfoList[i].numberUser}
+                        </small>
+
+                    </div>
+
+                </div>
+
+                <a href="tel:${contactInfoList[i].numberUser}" class="favorite-call">
+                    <i class="fas fa-phone"></i>
+                </a>
+
+            </div>
+
+            `;
+
+        }
+
+    }
+
+    if (container == "") {
+
+        container = "No favorites yet";
+
+    }
+
+    document.getElementById("favoriteList").innerHTML = container;
+
+}
+ function displayEmergency() {
+
+    var container = "";
+
+    for (var i = 0; i < contactInfoList.length; i++) {
+
+        if (contactInfoList[i].emergency == true) {
+
+            container += `
+            
+            <div class="favorite-item d-flex justify-content-between align-items-center mb-3">
+
+                <div class="d-flex align-items-center gap-2">
+
+                    <div class=" avatar-contact">
+                        ${contactInfoList[i].initials}
+                    </div>
+
+                    <div class="side-info">
+
+                        <h6 class="side-name">
+                            ${contactInfoList[i].nameUser}
+                        </h6>
+
+                        <small class="side-number">
+                            ${contactInfoList[i].numberUser}
+                        </small>
+
+                    </div>
+
+                </div>
+
+                <a href="tel:${contactInfoList[i].numberUser}" class="emergency-call">
+                    <i class="fas fa-phone"></i>
+                </a>
+
+            </div>
+
+            `;
+
+        }
+
+    }
+
+    if (container == "") {
+
+        container = "No emergency contacts";
+
+    }
+
+    document.getElementById("emergencyList").innerHTML = container;
+
+}
+function isContactExist(phone, currentIndex = -1){
+
+    for(var i=0; i<contactInfoList.length; i++){
+
+        if(
+            contactInfoList[i].numberUser == phone &&
+            i != currentIndex
+        ){
+
+            return true;
+
+        }
+
+    }
+
+    return false;
+
+}
+function updateStatistics() {
+
+  var favoriteCount = 0;
+  var emergencyCount = 0;
+
+  for (var i = 0; i < contactInfoList.length; i++) {
+
+    if (contactInfoList[i].favorite == true) {
+      favoriteCount++;
+    }
+
+    if (contactInfoList[i].emergency == true) {
+      emergencyCount++;
+    }
+
+  }
+
+  document.getElementById("favoriteContacts").innerHTML = favoriteCount;
+
+  document.getElementById("emergencyContacts").innerHTML = emergencyCount;
+
+  document.getElementById("totalContacts").innerHTML =
+    favoriteCount + emergencyCount;
+
+}
+ function toggleFavorite(index) {
+  contactInfoList[index].favorite =
+    !contactInfoList[index].favorite;
+
+  localStorage.setItem(
+    "containerContact",
+    JSON.stringify(contactInfoList)
+  );
+
+  displayContact();
+  displayFavorites();
+displayEmergency();
+  updateStatistics();
+}
+function toggleEmergency(index) {
+  contactInfoList[index].emergency =
+    !contactInfoList[index].emergency;
+
+  localStorage.setItem(
+    "containerContact",
+    JSON.stringify(contactInfoList)
+  );
+
+  displayContact();
+  displayFavorites();
+displayEmergency();
+  updateStatistics();
 }
